9c59344d10a206af38974f9bf5d2c2b0ef8cee58
wangjiqing <wjq2564331@126.com>
2023-12-03 22:52:57 +0800

    3-7 权限控制和行为日志

--- app/api/v1/content.js
+++ app/api/v1/content.js
@@ -1,24 +1,35 @@
 import { LinRouter } from 'lin-mizar';
 import { AddContentValidator, DeleteContentValidator, EditContentValidator } from '../../validators/content';
 import { ContentService } from '../../service/content';
+import { groupRequired } from '../../middleware/jwt';
+import { logger } from '../../middleware/logger';
 
 const contentApi = new LinRouter({
   prefix: '/v1/content'
 });
 
 /**
- * 新增期刊内容
+ * 新增期刊内容（加权控）
  */
-contentApi.post('/', async ctx => {
-  // 1. 参数校验
-  const v = await new AddContentValidator().validate(ctx);
-  // 2. 执行业务逻辑 && 3. 插入数据库
-  await ContentService.addContent(v.get('body'));
-  // 4. 返回成功
-  ctx.success({
-    msg: '期刊内容新增成功'
+contentApi.linPost(
+  'addContent',
+  '/', {
+    permission: '添加期刊内容',
+    module: '内容管理',
+    mount: true
+  },
+  groupRequired,
+  logger('{user.username}新增了一个期刊内容'),
+  async ctx => {
+    // 1. 参数校验
+    const v = await new AddContentValidator().validate(ctx);
+    // 2. 执行业务逻辑 && 3. 插入数据库
+    await ContentService.addContent(v.get('body'));
+    // 4. 返回成功
+    ctx.success({
+      msg: '期刊内容新增成功'
+    });
   });
-});
 
 /**
  * 查看期刊列表
@@ -31,16 +42,25 @@ contentApi.get('/', async ctx => {
 /**
  * 编辑期刊内容
  */
-contentApi.put('/:id', async ctx => {
-  const v = await new EditContentValidator().validate(ctx);
-  const id = v.get('path.id');
-  const params = v.get('body');
+contentApi.linPut(
+  'editContent',
+  '/:id',
+  {
+    permission: '编辑期刊内容',
+    module: '内容管理',
+    mount: true
+  },
+  groupRequired,
+  async ctx => {
+    const v = await new EditContentValidator().validate(ctx);
+    const id = v.get('path.id');
+    const params = v.get('body');
 
-  await ContentService.editContent(id, params);
-  ctx.success({
-    msg: '期刊内容更新成功'
+    await ContentService.editContent(id, params);
+    ctx.success({
+      msg: '期刊内容更新成功'
+    });
   });
-});
 
 /**
  * 删除期刊内容
